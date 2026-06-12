28ae4c99bccd2915ab2ccdf162f7eff7feb93fc3
tryte <tryte001@gmail.com>
2026-06-12 16:16:32 +0800

    update

--- virtio-drivers/src/input.rs
+++ virtio-drivers/src/input.rs
@@ -15,19 +15,73 @@ const QUEUE_EVENT: usize = 0;
 const QUEUE_STATUS: usize = 1;
 
 bitflags! {
+    ///
+    /// 输入设备功能特性位
+    ///
+    /// @author: tryte
+    ///
+    /// @date: 2026/6/12
     struct Feature:u64 {
+        /// 当 avail ring 被消费空时通知驱动
         const NOTIFY_ON_EMPTY    = 1 << 24;
+        /// 支持任意 Descriptor 链布局（兼容旧版 VirtIO）
         const ANY_LAYOUT         = 1 << 27;
+        /// 支持间接 Descriptor，可用一个 Desc 引用一张 Desc 表
+        /// 正常情况：Descriptor Table 0、1、2、3...
+        /// 如果一次请求需要 100 个 desc 需要占 100 个槽，开启后：
+        /// Descriptor Table 0、1、2 ... 99
+        ///                  ↓
+        ///            Indirect Table
+        /// 每个 Descriptor 都可以维护一个间接表，减少 Descriptor Table 消耗
         const RING_INDIRECT_DESC = 1 << 28;
+        /// 支持 Event Index，减少不必要的中断通知
+        /// 默认：每完成一次请求 -> 中断一次，开启后：
+        /// 完成到指定 idx 才中断
+        /// 例如：处理到 used_idx=100 再通知，减少中断风暴
         const RING_EVENT_IDX     = 1 << 29;
+        /// 保留位，未使用
         const UNUSED             = 1 << 30;
+        /// 表示是否符合 VirtIO 1.0+ 规范（现代 VirtIO 设备）
         const VERSION_1          = 1 << 32;
-
+        /// 支持平台 DMA/IOMMU 地址转换，设备访问内存是否需要经过平台 DMA/IOMMU，驱动必须根据这个标志位正确处理 DMA 地址转换
         const ACCESS_PLATFORM   = 1 << 33;
+        /// 支持 Packed VirtQueue 格式
+        ///
+        /// 传统 VirtQueue：
+        ///     Descriptor Table
+        ///     Avail Ring
+        ///     Used Ring
+        ///
+        /// Packed VirtQueue：
+        ///  +------+    +-----------+
+        ///  | desc |    | addr      |
+        ///  |      | -> | len       |
+        ///  |      |    | flags     |
+        ///  |      |    | owner bit | -> owner bit 类似：0 = Driver拥有；1 = Device拥有
+        ///  +------+    +-----------+
+        ///  | desc |
+        ///  +------+
+        ///
+        /// Packed VirtQueue 将三个队列融合成一个队列，每个队列元素包含addr、len、flags、owner bit，
+        /// 这样设备在获取驱动任务的时候就不需要：读 avail_idx -> 读 avail ring[idx] -> 读 desc -> 写 used
+        /// 可以提高 cache 缓存命中率和减少内存读取存储操作
+        ///
         const RING_PACKED       = 1 << 34;
+        /// 保证请求按提交顺序完成
         const IN_ORDER          = 1 << 35;
+        /// 遵循平台内存排序规则，与内存屏障有关，表示设备是否遵循平台内存排序规则
         const ORDER_PLATFORM    = 1 << 36;
+        /// 支持 SR-IOV 虚拟化功能，即：一个物理设备 -> 多个虚拟设备，如：
+        /// 物理网卡
+        /// ├─ VF0
+        /// ├─ VF1
+        /// ├─ VF2
+        /// 常见于高性能虚拟化
         const SR_IOV            = 1 << 37;
+        /// 通知时可携带额外队列信息，优化 MMIO Notify。传统：notify -> 设备再查队列信息，开启后：notify 时直接带队列信息。例如：
+        /// queue id
+        /// next avail idx
+        /// 减少一次内存访问
         const NOTIFICATION_DATA = 1 << 38;
     }
 }
